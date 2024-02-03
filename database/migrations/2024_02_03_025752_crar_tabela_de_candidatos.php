0b05c1490330d8dbb230f9c21b88a3b4c87e1e7d
Marcus <marcus.vieiraleal94@gmail.com>
2024-02-03 00:55:43 -0300

    Fazendo Formulario

---
+++ database/migrations/2024_02_03_025752_crar_tabela_de_candidatos.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create("candidatos", function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("telefone");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::drop("candidatos");
+    }
+};
