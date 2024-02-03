01ebe846ea06c514a721139d74118d954006407a
Marcus <marcus.vieiraleal94@gmail.com>
2024-02-03 16:08:45 -0300

    Atualização campo do formulario e css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,18 +7,25 @@
     <title>Crud</title>
 </head>
 <body>
-    {{-- BOOTSTRAP 5 --}}
+    <!-- BOOTSTRAP 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
-    <form>
+    <!-- CSS -->
+    <link rel="stylesheet" href="/css/style.css">
+
+    <form action="/cadastrar" method="post">
+        @csrf
         <div class="row">
           <div class="col">
             <input type="text" class="form-control" placeholder="Seu Nome..." name="nome">
           </div>
+
           <div class="col">
             <input type="text" class="form-control" placeholder="Seu Telefone..." name="telefone">
           </div>
+
+          <button class="btn btn-primary">Cadastrar</button>
         </div>
       </form>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Candidato;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('/cadastrar', function (Request $informacoes) {
+    Candidato::create([
+        'nome' => $informacoes->nome,
+        'telefone' => $informacoes->telefone
+    ]);
+    echo 'Candidato cadastrado com sucesso!';
 });
